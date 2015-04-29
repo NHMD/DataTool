2848b5a87c6f3776813ffe307aa8543bf48bd98d
Thomas Stjernegaard Jeppesen <t.stjernegaard@snm.ku.dk>
2015-04-29 15:03:16 +0200

    Closes #40

--- server/components/datamapper/index.js
+++ server/components/datamapper/index.js
@@ -11,146 +11,103 @@ var Promise = require("bluebird");
 var MongoDB = require("./nativeMongoInstance");
 var _ = require('lodash');
 
-/*
-var mappings = {
 
-	"Field Number": {
-		fieldName: "FieldNumber",
-		tableName: "Collectionobject"
-	},
-	"Start Date": {
-		fieldName: "StartDate",
-		tableName: "Collectingevent"
-	},
-	"Collector First Name1": {
-		refTable: "Collector",
-		fieldName: "FirstName",
-		tableName: "Agent"
-	},
-	"Collector Last Name1": {
-		refTable: "Collector",
-		fieldName: "LastName",
-		tableName: "Agent"
-	},
-	"Collector Middle1": {
-		refTable: "Collector",
-		fieldName: "MiddleInitial",
-		tableName: "Agent"
-	}
-
-	"Determiner Last Name1": {
-		refTable: "Determintation",
-		fieldName: "LastName",
-		tableName: "Agent"
-	},
-	"Determiner First Name1": {
-		refTable: "Determintation",
-		fieldName: "FirstName",
-		tableName: "Agent"
-	},
-	"Determiner Middle1": {
-		refTable: "Determintation",
-		fieldName: "MiddleInitial",
-		tableName: "Agent"
-	},
-	// "Genus1": {fieldName: , tableName: },
-	// "Species1": {fieldName: , tableName: },
-	"Remarks 1": {
-		fieldName: "Remarks",
-		tableName: "Collectionobject"
-	},
-	// "Gene Sequence 1": {fieldName: , tableName: }
-};
-*/
 
 exports.insertIntoSpecify = function(collectionName, csvimport) {
-	
+
 	var mappings = csvimport.mapping;
-	
+
 	csvimport.specifycollection
-	
+
 	return MongoDB.connect()
 		.then(function(db) {
 			return db.collection(collectionName).findAsync(
-				
+
 			);
 		})
 		.then(function(result) {
-			
-		//	var instances = [];
-			result.each(function(err,elm){
-				if (err) {throw err};
-				
+
+			//	var instances = [];
+			result.each(function(err, elm) {
+				if (err) {
+					throw err
+				};
+
 				var instances = {};
-				
+
 				for (var key in elm) {
-					if(key !== "_id" && !isTree(mappings[key].tableName))
-					{
-						if(mappings[key].refTable && !instances[mappings[key].refTable]){
-							
+					if (key !== "_id" && !isTree(mappings[key].tableName)) {
+						if (mappings[key].refTable && !instances[mappings[key].refTable]) {
+
 							instances[mappings[key].refTable] = {};
-							
+
 							instances[mappings[key].refTable][mappings[key].tableName] = {};
-						}
-						
-						else if(!mappings[key].refTable && mappings[key].tableName && !instances[mappings[key].tableName] ){
+						} else if (!mappings[key].refTable && mappings[key].tableName && !instances[mappings[key].tableName]) {
 							instances[mappings[key].tableName] = {}
 						};
-						
-						if(mappings[key].refTable){
+
+						if (mappings[key].refTable) {
 							instances[mappings[key].refTable][mappings[key].tableName][mappings[key].fieldName] = elm[key];
+						} else {
+							instances[mappings[key].tableName][mappings[key].fieldName] = elm[key];
 						}
-						else {
-						instances[mappings[key].tableName][mappings[key].fieldName] = elm[key];
-					}
-						
-					} else if(key !== "_id" && isTree(mappings[key].tableName)){
-						
+
+					} else if (key !== "_id" && isTree(mappings[key].tableName)) {
+
 						//TODO map a single taxon to the tree ???
 					};
-					
-		
+
+
 				};
-				specifyModel['Collection'].findOne({collectionId: csvimport.specifycollection.collectionId})
-				.then(function(collection){
-					console.log("XXXXXcollection")
-				//	var collectionObj = _.merge(instances['Collectionobject'], {CollectionMemberID: collection.collectionId, CollectionID: collection.collectionId})
-					return specifyModel['Collectionobject'].create(_.merge(instances['Collectionobject'], {CollectionMemberID: collection.collectionId, CollectionID: collection.collectionId}))
+				specifyModel['Collection'].findOne({
+					collectionId: csvimport.specifycollection.collectionId
 				})
-				.then(function(collectionObject){
-					console.log("XXXXXcollectionObject"+JSON.stringify(collectionObject))
-					var det = _.merge(instances['Determination'],{CollectionMemberID: collectionObject.CollectionMemberID, CollectionObjectID : collectionObject.CollectionobjectID });
-					
-					console.log("#######   "+JSON.stringify(det))
-					return specifyModel['Determination'].create(det)
-				})
-				.then(function(determination){
-					console.log("XXXXXdetermination")
-					return [specifyModel['Agent'].create(instances['Determination']['Agent']), determination]
-				})
-				.spread(function(agent, determination){
-					console.log("XXXXXagent :")
-					return determination.addAgent(agent);
-				})
-				.then(function(){
-					console.log("###############################DONE##################")
-				})
-				.catch(function(err){
+					.then(function(collection) {
+						console.log("XXXXXcollection")
+						//	var collectionObj = _.merge(instances['Collectionobject'], {CollectionMemberID: collection.collectionId, CollectionID: collection.collectionId})
+						return specifyModel['Collectionobject'].create(_.merge(instances['Collectionobject'], {
+							CollectionMemberID: collection.collectionId,
+							CollectionID: collection.collectionId
+						}))
+					})
+					.then(function(collectionObject) {
+						console.log("XXXXXcollectionObject" + JSON.stringify(collectionObject))
+						var det = _.merge(instances['Determination'], {
+							CollectionMemberID: collectionObject.CollectionMemberID,
+							CollectionObjectID: collectionObject.CollectionobjectID
+						});
+
+						console.log("#######   " + JSON.stringify(det))
+						return specifyModel['Determination'].create(det)
+					})
+					.then(function(determination) {
+						console.log("XXXXXdetermination")
+						return [specifyModel['Agent'].create(instances['Determination']['Agent']), determination]
+					})
+					.spread(function(agent, determination) {
+						console.log("XXXXXagent :")
+						return determination.addAgent(agent);
+					})
+					.then(function() {
+						console.log("###############################DONE##################")
+					})
+					.
+				catch (function(err) {
 					console.log(err.message);
 					throw err;
 				})
-			//	var collectionObject = specifyModel['Collectionobject'].build(instances['Collectionobject']);
-				
-			//	var determination = specifyModel['Determination'].build(instances['Determination']);
-				
-			//	console.log(JSON.stringify(determination))
-				
-			//	console.log(JSON.stringify(instances))
-				
+				//	var collectionObject = specifyModel['Collectionobject'].build(instances['Collectionobject']);
+
+				//	var determination = specifyModel['Determination'].build(instances['Determination']);
+
+				//	console.log(JSON.stringify(determination))
+
+				//	console.log(JSON.stringify(instances))
+
 			})
-		
-			
-		//	return Promise.all(instances);
+
+
+			//	return Promise.all(instances);
 		})
 		.
 	catch (function(err) {
@@ -215,217 +172,236 @@ exports.aggregateAndPersist = function(collectionName, mappings, model) {
 }
 
 exports.aggregateTreeAndPersist = function(collectionName, mappings, model, discipline) {
-	
+
 	// flattenDsicipline is attaching the StorageTreeDef on same level as other trees
 	discipline = flattenDiscipline(discipline);
 
 	var treeDef = model + "treedefitem";
-	
+
 	return SpecifyModelDescription.findOne({
 		table: model.toLowerCase()
-	}).exec().then(function(m){
-				
-		//-----------------
+	}).exec().then(function(m) {
+		
+		
 		var treeIDName = m.idColumn;
 		var mdlName = treeIDName.split("ID")[0];
 		var treeDefName = mdlName + "TreeDefID";
-		var treeDefItemName = mdlName + "TreeDefItemID"; 
-		//----------------------------------------------------------	
-	
-	var ranks = _.omit(mappings, function(value, key, object) {
-		return object["rankName"] !== undefined;
+		var treeDefItemName = mdlName + "TreeDefItemID";
+
+		var treeDefNames = {
+			treeIDName: treeIDName,
+			mdlName: mdlName,
+			treeDefName: treeDefName,
+			treeDefItemName: treeDefItemName
+		}
+
+		var ranks = _.omit(mappings, function(value, key, object) {
+		//	console.log("tredefitem ????? "+ JSON.stringify(object))
+			return object[key]["TreeDefItem"] === undefined;
 		});
-	
-	var where = {
-		Name: _.map(ranks, function(val, key) {
-			return ranks[key].rankName
-		})
-	};
-	where[treeDefName] = discipline[treeDefName];
 
-	return specifyModel[treeDef].findAll({
-		where: where,
-		order: 'RankID ASC'
-	})
-
-	.then(function(treedefitems) {
-
-		var rankMappings = {};
-
-		for (var i = 0; i < treedefitems.length; i++) {
-			rankMappings[treedefitems[i].Name] = {
-				RankID: treedefitems[i].RankID
-			};
-			rankMappings[treedefitems[i].Name][treeDefItemName] = treedefitems[i][treeDefItemName];
-		};
-
-		var aggregation = {}
-
-		for (var key in mappings) {
-			//if (rankMappings[mappings[key].rankName]) aggregation[key] = "$" + key;
-			aggregation[key] = "$" + key;;
-		};
-
-		// Used to create a dummy tree item to prevent Ophants
-		var rootWhere = {};
-		rootWhere[treeDefName] = discipline[treeDefName];
-		rootWhere['RankID'] = 0;
-		var treeRoot = specifyModel[model].findOne({where: rootWhere})
-		var secondLevelTReeDefItem = treedefitems[1];
-		
-		return [MongoDB.connect(), aggregation, collectionName, rankMappings, treeRoot, secondLevelTReeDefItem];
-	})
-	
-	
-	.spread(function(db, aggregation, collectionName, rankMappings, treeRoot, secondLevelTReeDefItem) {
-		
-		var dummyParent ={RankID: secondLevelTReeDefItem.RankID, ParentID: treeRoot[treeIDName], Name: 'IMPORTED' };
-		dummyParent[treeDefName] = treeRoot[treeDefName];
-		dummyParent[treeDefItemName] = secondLevelTReeDefItem[treeDefItemName];
-		return [db.collection(collectionName).aggregateAsync(
-			[{
-				"$group": {
-					"_id": aggregation
-				}
-			}]
-		), model, rankMappings, specifyModel[model].create(dummyParent) /* specifyModel.sequelize.transaction({isolationLevel : specifyModel.sequelize.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED})*/]
-	})
-
-	.spread(function(result, model, rankMappings, dummyParent) {
-		
-
-	
-
-		var instances = [];
-		
-		// Attributes which are not associated with tree rank will be attached to the highest resoluted tree node, e.g. species if we have: Family, Genus, Species in the flat CSV:
-		var nonRankMappings  = _.omit(mappings, function(value, key, object) {
-			
-		return object[key]["rankName"] !== undefined;
-});
-		for (var i = 0; i < result.length; i++) {
-			instances.push(findAndInsert(result[i], model, rankMappings, mappings, treeDefItemName, treeDefName, discipline, nonRankMappings, dummyParent));
-
-
-		}
-		return [Promise.all(instances), dummyParent];
-	}).spread(function(instances, dummyParent){
-		
-	return	[specifyModel[model].count({ where: {ParentID: dummyParent[treeIDName]}}), dummyParent]
-		
-	}).spread(function(c, dummyParent) {
-		if (c === 0){
-			return dummyParent.destroy();
-		}
-		else {
-			return true;
-		}
-		})
-		.
-	catch (function(err) {
-			console.log(err.message)
-			console.log(err.errors)
-		throw err;
-	});
-
-});
-
-}
-
-function findAndInsert(result, model, rankMappings, mappings, treeDefItemName, treeDefName, discipline, nonRankMappings, dummyParent) {
-
-	var mapped = [];
-	
-	for (var key in result._id) {
-		if(mappings[key].rankName){
-		var elm = {
-			RankID: rankMappings[mappings[key].rankName].RankID,
-			Name: result._id[key]
-		};
-
-		elm[treeDefItemName] = rankMappings[mappings[key].rankName][treeDefItemName];
-		mapped.push(elm)
-	}
-		
-	};
-
-	mapped.sort(function(a, b) {
-		return a.RankID > b.RankID;
-	});
-	// Attributes which are not associated with tree rank will be attached to the highest resoluted tree node, e.g. species if we have: Family, Genus, Species in the flat CSV:
-	
-	for (var key in nonRankMappings) {
-		
-		mapped[mapped.length-1][nonRankMappings[key].fieldName] = result._id[key];
-	}
-	
-	return Promise.reduce(mapped, function(previous, query) {
-
-		if (previous !== 0) {
-
-			query = _.merge(query, {
-				ParentID: previous[model + "ID"]
+		ranks = _.map(ranks, function(value, key) {
+			return _.merge(_.omit(value, 'table'), {
+				mongoColumnName: key
 			});
-		};
+		});
+		
+		ranks.sort(function(a, b) {
+			return a.TreeDefItem.RankID - b.TreeDefItem.RankID;
+		});
 
-		query[treeDefName] = discipline[treeDefName];
-		var transaction = specifyModel.sequelize.transaction({isolationLevel : specifyModel.sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE});
-		return transaction.then(function(t){
-			
-			return [specifyModel[model].findOrCreate({
-			
-				where: query
-			}, {transaction: t}), t]
-			
-			
-		})
-		.spread(function(treenodeInitialized, t) {
-			var treenode = treenodeInitialized[0];
-			var initialized = treenodeInitialized[1];
-			if(initialized){
-				console.log("initalized ******************* "+JSON.stringify(treenode.get()))
-				if(!treenode.ParentID){
-					treenode.ParentID = dummyParent[model + "ID"];
-				}
-				return [treenode.save({transaction: t}), t];
-			} else 
-			{
-				return [treenode, t];
-			}
 		
-		}).spread(function(treenode, t){
-			
-			t.commit();
-			return treenode;
-			
+		var where = {};
+		where[treeDefNames.treeDefName] = discipline[treeDefNames.treeDefName];
+
+
+		return specifyModel[treeDef].findAll({
+			where: where,
+			order: 'RankID ASC'
 		})
-		
+			.then(function(alltreedefitems) {
+
+
+				// Used to create a dummy tree item to prevent Ophants
+				var rootWhere = {};
+				rootWhere[treeDefName] = discipline[treeDefName];
+				rootWhere['RankID'] = 0;
+				var treeRoot = specifyModel[model].findOne({
+					where: rootWhere
+				})
+				var secondLevelTReeDefItem = alltreedefitems[1];
+				return [treeRoot, secondLevelTReeDefItem]
+			})
+			.spread(function(treeRoot, secondLevelTReeDefItem) {
+
+				var dummyParent = {
+					RankID: secondLevelTReeDefItem.RankID,
+					ParentID: treeRoot[treeIDName],
+					Name: 'IMPORTED'
+				};
+				dummyParent[treeDefName] = treeRoot[treeDefName];
+				dummyParent[treeDefItemName] = secondLevelTReeDefItem[treeDefItemName];
+				return specifyModel[model].create(dummyParent);
+			})
+			.then(function(dummyParent) {
+
+				return [MongoDB.connect(), dummyParent]
+			})
+
+		.spread(function(db, dummyParent) {
+
+			aggregateTreeLevel(db, ranks, collectionName, treeDefNames, model, dummyParent)
+
+		})
 			.
 		catch (function(err) {
+			
 			console.log(err.message)
-			console.log(err.errors)
 			throw err;
-		});;
+		});
+
+	});
+}
+
+/*
+
+This function will take a sorted array of tree ranklevels and produce an array og group objects for mongodbs aggregation pipeline
+ 
+*/
+
+function getAggregationQuery(ranks) {
+
+	var retval = [];
+	var firstAggr = {
+		$group: {
+			_id: {}
+		}
+	};
+	for (var i = 0; i < ranks.length; i++) {
+
+		firstAggr.$group._id[ranks[i].mongoColumnName] = "$" + ranks[i].mongoColumnName;
 
 
-	}, 0);
+	}
+	retval.push(firstAggr);
+
+	for (var i = 1; i < ranks.length; i++) {
+		var aggr = {
+			$group: {
+				_id: {}
+			}
+		};
+		aggr.$group[ranks[ranks.length - i].mongoColumnName] = {
+			$addToSet: {}
+		};
+		aggr.$group[ranks[ranks.length - i].mongoColumnName].$addToSet[ranks[ranks.length - i].mongoColumnName] = "$_id." + ranks[ranks.length - i].mongoColumnName;
+		if (i > 1) {
+			aggr.$group[ranks[ranks.length - i].mongoColumnName].$addToSet[ranks[ranks.length - i + 1].mongoColumnName] = "$" + ranks[ranks.length - i + 1].mongoColumnName;
+		}
+		for (var j = 0; j < ranks.length - i; j++) {
+
+			aggr.$group._id[ranks[j].mongoColumnName] = "$_id." + ranks[j].mongoColumnName;
+
+
+
+
+		}
+		retval.push(aggr);
+	}
+	return retval;
+
+}
+
+function createTreeNode(node, parent, rankLevel, ranks, treeDefNames, model, dummyParent) {
+	
+	var name = (rankLevel === 0) ? node._id[ranks[rankLevel].mongoColumnName] : node[ranks[rankLevel].mongoColumnName];
+	var where = {
+		Name: name,
+		RankID: ranks[rankLevel].TreeDefItem.RankID
+	};
+	where[treeDefNames.treeDefItemName] = ranks[rankLevel].TreeDefItem[treeDefNames.treeDefItemName];
+	where[treeDefNames.treeDefName] = ranks[rankLevel].TreeDefItem[treeDefNames.treeDefName];
+	if (parent !== dummyParent) {
+		where['ParentID'] = parent[treeDefNames.treeIDName];
+	}
+
+return	specifyModel[model].findOrCreate({
+		where: where
+	})
+		.spread(function(treenode, created) {
+		// If we are at the uppermost treelevel, check if parent exists, or use the dummy	
+			if(created && !treenode.ParentID){
+				treenode.ParentID = dummyParent[treeDefNames.treeIDName];
+				treenode.save();
+			}
+		// stop the recursion, leafs are reached
+			if (rankLevel === ranks.length - 1) {
+			
+				return treenode;
+			} 
+			// continue to next tree level
+			else {
+				var children = node[ranks[rankLevel + 1].mongoColumnName];
+				if (children) {
+					for (var i = 0; i < children.length; i++) {
+
+						createTreeNode(children[i], treenode, rankLevel + 1, ranks, treeDefNames, model, dummyParent);
+					}
+				}
+			}
+
+		});
 
 
 }
 
-function flattenDiscipline(discipline){
-	
+function aggregateTreeLevel(db, ranks, collectionName, treeDefNames, model, dummyParent) {
+
+	var aggregationQuery = getAggregationQuery(ranks);
+
+	return db.collection(collectionName).aggregateAsync(aggregationQuery, {
+                                     allowDiskUse: true
+                                   })
+		.then(function(result) {
+			var promises = [];
+			for (var i = 0; i < result.length; i++) {
+				promises.push(createTreeNode(result[i], dummyParent, 0, ranks, treeDefNames, model, dummyParent));
+			}
+			
+			return Promise.all(promises)
+		}).then(function(){
+			return specifyModel[model].findOne({
+					where: {ParentID : dummyParent[treeDefNames.treeIDName] }
+				})
+		}).then(function(treenode){
+			if(!treenode){
+				dummyParent.destroy();
+			}
+		}).
+		catch (function(err) {
+			console.log("!!!!!!!!!!!!!!!!!!!!!!")
+			console.log(err.message)
+			throw err;
+		});
+
+
+}
+
+
+
+function flattenDiscipline(discipline) {
+
 	var treeDefPattern = /TreeDefID/;
 	var flattenedDiscipline = {};
-	_.each(discipline, function(value, key){
-		if(treeDefPattern.test(key)){
+	_.each(discipline, function(value, key) {
+		if (treeDefPattern.test(key)) {
 			flattenedDiscipline[key] = value;
 		}
 	})
-	if(discipline.division && discipline.division.institution && discipline.division.institution.StorageTreeDefID)	{
+	if (discipline.division && discipline.division.institution && discipline.division.institution.StorageTreeDefID) {
 		flattenedDiscipline.StorageTreeDefID = discipline.division.institution.StorageTreeDefID;
 	};
-	
+
 	return flattenedDiscipline;
 
 }
