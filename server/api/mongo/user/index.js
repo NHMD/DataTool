e8261176863396c9d0c501a9a222e4402525ed56
davidkonrad <davidkonrad@gmail.com>
2015-05-04 11:29:13 +0200

    Edited user permissions. Closes #45

--- server/api/mongo/user/index.js
+++ server/api/mongo/user/index.js
@@ -7,12 +7,12 @@ var auth = require('../../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', auth.hasRole('Manager'), controller.index);
+router.get('/', auth.isAuthenticated(), controller.index);
 router.delete('/:id', auth.hasRole('Manager'), controller.destroy);
 router.get('/me', auth.isAuthenticated(), controller.me);
 router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
 router.get('/:id', auth.isAuthenticated(), controller.show);
-router.post('/', controller.create);
+router.post('/', auth.hasRole('Manager'), controller.create);
 router.put('/:id', auth.hasRole('Manager'), controller.update);
 
 
